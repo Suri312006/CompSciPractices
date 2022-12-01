d3974a4e8e276c4f2eebeb9732c3b280df6e2fd1
wlxb109s15 <s200766@scusd.net>
2022-12-01 10:10:25 -0800

    I got that Dawg in me.

---
+++ Dog.java
@@ -0,0 +1,20 @@
+public class Dog {
+    String name;
+    String color;
+    int weight;
+    boolean fed;
+
+    public Dog(String name, String color, int weight, boolean fed){
+        this.name = name;
+        this.color = color;
+        this.weight = weight;
+        this.fed = fed;
+    }
+
+    public void feed(int amount){
+        if(!fed){
+            fed = true;
+            weight += amount;
+        }
+    }
+}
---
+++ WhatsPoppin.java
@@ -0,0 +1,7 @@
+public class WhatsPoppin {
+    public static void main(String[] penis){
+        Dog fido = new Dog("fido", "brown", 50, false);
+        fido.feed(3);
+        System.out.println(fido.isfed(););
+    }
+}
