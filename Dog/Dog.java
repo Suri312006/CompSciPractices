ca3e6dd377d0837e97243faed80eb96dd83ffa28
wlxb109s15 <s200766@scusd.net>
2022-12-02 09:35:14 -0800

    that boy fuckin died

--- Dog/Dog.java
+++ Dog/Dog.java
@@ -1,17 +1,45 @@
 package Dog;
 
 public class Dog {
-    String name;
-    String color;
-    int weight;
-    boolean fed;
+    //first you set up all the fields you want
+    private String name;
+    private String color;
+    private int weight;
+    private boolean fed;
 
+   private  boolean alive = true;
+
+    //constructor (should loook exactly like this)
     public Dog(String name, String color, int weight, boolean fed){
         this.name = name;
         this.color = color;
         this.weight = weight;
         this.fed = fed;
     }
+    // constructor for stray
+    public Dog(String color, int weight) {
+        this.name = "Stray";
+        this.color = color;
+        this.weight = weight;
+        this.fed = false;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getColor(){
+        return color;
+    }
+    public int getWeight(){
+        return weight;
+    }
+    public boolean getFed(){
+        return fed;
+    }
+
+    public boolean isAlive(){
+        return alive;
+    }
+
 
     public void feed(int amount){
         if(!fed){
@@ -20,10 +48,40 @@ public class Dog {
         }
     }
 
-    public String isfed(){
-      if(fed){
-        return "Yes, he is fed. He now weights "+weight;
-      }
-      return "That boy starvin";
+    public void run(int distance){
+        double temp = weight - distance/100;
+        if(temp < 1){
+            alive = false;
+            weight = 0;
+        }
+        else{
+            weight = (int) temp;
+        }
+        fed = false;
     }
+
+    public String isFed(){
+      if(!alive){
+          return "That boy " + name +" is fucking dead";
+      }
+      else if(fed){
+        return "Yes, " + name+ " is fed. They now weigh "+weight;
+      }
+      return "That boy "+name+" is starvin";
+    }
+
+    public void bite(Dog bitten, int chunk){
+        int temp = bitten.weight - chunk;
+        if(temp < bitten.weight / 2){
+            bitten.alive = false;
+        }
+        bitten.weight -= chunk;
+
+        this.weight += chunk;
+        this.fed = true;
+
+    }
+
+
+
 }
--- Dog/WhatsPoppin.java
+++ Dog/WhatsPoppin.java
@@ -1,9 +1,35 @@
 package Dog;
 
+import java.util.*;
+
 public class WhatsPoppin {
     public static void main(String[] penis){
         Dog fido = new Dog("fido", "brown", 50, false);
-        fido.feed(3);
-        System.out.println(fido.isfed());
+        Dog dumbass = new Dog("dumbass", "red", 100, false);
+        Scanner input = new Scanner(System.in);
+
+        /*
+        Dog[] Dogs = {fido, dumbass};
+        for(Dog dog : Dogs){
+            System.out.print("Do you want to feed "+dog.name+" ? (Enter y or n): ");
+            String response = input.nextLine();
+            if(response.equals("y")){
+                System.out.print("How much do you want to feed "+dog.name+" ? ");
+                int fedAmount = input.nextInt();
+                dog.feed((fedAmount));
+            }
+        }
+        */
+
+        fido.bite(dumbass, 70);
+
+        if(!dumbass.isAlive()){
+            System.out.println("dumbass fucking died ");
+        }
+
+        System.out.print(dumbass);
+
+
+
     }
 }
